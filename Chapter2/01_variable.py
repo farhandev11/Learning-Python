db7cd1e0316fa27cd7005d715f6f36a70f7dee6a
farhandev11 <farhandev857@gmail.com>
2025-05-31 16:05:15 +0500

    farhan ny mujtaba ko abu banya

--- Chapter2/01_variable.py
+++ Chapter2/01_variable.py
@@ -1,3 +1,10 @@
 a= 30 # variables = container to store a value.
 b= "harry" # keywords = reserved words in python
 c= 71.22 # identifiers = class/function/variable name
+
+"""hello this is mujtaba making repo for farhan gandu"""
+
+def farhan_gandu(name,gender):
+    return name , gender
+
+print(farhan_gandu("abu mujtaba" , "shemale"))
