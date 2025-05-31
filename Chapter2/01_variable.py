9113425b7e0ed942bb837917351e23e6b543b57f
farhandev11 <farhandev857@gmail.com>
2025-05-31 16:07:58 +0500

    Update 01_variable.py

--- Chapter2/01_variable.py
+++ Chapter2/01_variable.py
@@ -2,9 +2,9 @@ a= 30 # variables = container to store a value.
 b= "harry" # keywords = reserved words in python
 c= 71.22 # identifiers = class/function/variable name
 
-"""hello this is mujtaba making repo for farhan gandu"""
 
-def farhan_gandu(name,gender):
+def farhan(name,gender):
+    """hello this is mujtaba making repo for farhan"""
     return name , gender
 
-print(farhan_gandu("abu mujtaba" , "shemale"))
+print(farhan("farhan" , "shemale"))
